73af37e574201b1b4a8739debc200851eccac830
livialyrio <139237217+livialyrio@users.noreply.github.com>
2023-11-03 16:56:17 -0300

    Create StarWarsController.java

---
+++ StarWarsController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("StarWarsController")
+
+public class StarWarsController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        this.doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String nome = request.getParameter("nome");
+        String sobrenome = request.getParameter("sobrenome");
+        String nomeCerto = "luke";
+        String sobrenomeCerto = "Skywalker";
+        System.out.println("Criado: " + nome + " " + sobrenome);
+    }
+
+}
